2155a29e3d030d71f0d61840e87d78994370d4bc
Alex Mant <al.mantione@gmail.com>
2021-11-27 16:44:40 +0100

    déclaration des elements du dom

---
+++ starterOnly/Js/form.js
@@ -0,0 +1,9 @@
+/* BEGIN - DOM ELEMENTS*/
+const formElement = document.getElementById("form");
+const firstNameElement = document.getElementById("first");
+const lastNameElement = document.getElementById("last");
+const emailElement = document.getElementById("email");
+const birthDateElement = document.getElementById("birthdate");
+const quantityTournamentElement = document.getElementById("quantity");
+const signUpElement = document.querySelector(".btn-submit");
+/* END - DOM ELEMENTS*/
