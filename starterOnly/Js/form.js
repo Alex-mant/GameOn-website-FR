6c820e11d5777b9afa3beb9d86d5f2de3e211343
Alex Mant <al.mantione@gmail.com>
2021-11-27 16:45:28 +0100

    Ajout des elements messages erreur #issue3

--- starterOnly/Js/form.js
+++ starterOnly/Js/form.js
@@ -6,4 +6,12 @@ const emailElement = document.getElementById("email");
 const birthDateElement = document.getElementById("birthdate");
 const quantityTournamentElement = document.getElementById("quantity");
 const signUpElement = document.querySelector(".btn-submit");
+/* BEGIN - ERROR MESSAGE ELEMENTS */
+const errorFirstNameElement = document.querySelector("p.error-fName");
+const errorLastNameElement = document.querySelector("p.error-lName");
+const errorEmailElement = document.querySelector("p.error-eMail");
+const errorBirthDateElement = document.querySelector("p.error-bDate");
+const errorCityElement = document.querySelector("p.error-city");
+const errorQuantityElement = document.querySelector("p.error-qValue");
+/* END - ERROR MESSAGE ELEMENTS*/
 /* END - DOM ELEMENTS*/
