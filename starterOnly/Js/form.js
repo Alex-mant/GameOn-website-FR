da185bff93af9a679587e51719662629eca4a59d
Alex Mant <al.mantione@gmail.com>
2021-11-30 10:51:50 +0100

    refacto et ajout fermeture modalconfirm au click btn

--- starterOnly/Js/form.js
+++ starterOnly/Js/form.js
@@ -1,33 +1,11 @@
-/* BEGIN - DOM ELEMENTS*/
-const elements = {
-  form : document.getElementById("form"),
-  firstName : document.getElementById("first"),
-  lastName : document.getElementById("last"),
-  email : document.getElementById("email"),
-  birthDate : document.getElementById("birthdate"),
-  quantityTournament : document.getElementById("quantity"),
-  checkCity : document.querySelectorAll(".city-location"),
-  signUp : document.querySelector(".btn-submit"),
-  modalForm : document.querySelector(".modal-body form"),
-  error : {
-    firstName : document.querySelector("p.error-fName"),
-    lastName : document.querySelector("p.error-lName"),
-    email : document.querySelector("p.error-eMail"),
-    birthDate : document.querySelector("p.error-bDate"),
-    city : document.querySelector("p.error-city"),
-    quantityTournament : document.querySelector("p.error-qValue")
-  }
-};
-/* END - DOM ELEMENTS*/
-
-/* BEGIN - Fonction reset */
+// BEGIN - Fonction reset
 const reset = () => {
   elements.modalForm.reset();
   citiesSelected = [];
 };
-/* END - Fonction reset */
+// END - Fonction reset
 
-/*BEGIN - Selection des villes en fonction de la valeur répondue à "combien de tournoi ?" */
+// BEGIN - Selection des villes en fonction de la valeur répondue à "combien de tournoi ?"
 let citiesSelected = [];
 const ensureTownChecked = () => elements.checkCity.forEach(p => p.checked = citiesSelected.includes(p));
 
@@ -44,9 +22,9 @@ const townSelector = place => {
     ensureTownChecked();
   });
 }
-/*END - Selection des villes*/
+// END - Selection des villes
 
-/* BEGIN - Event Validation */
+// BEGIN - Event Validation
 const signUpValidationEvent_ = (event) => {
   event.preventDefault();
   const validations = [
@@ -65,9 +43,9 @@ const signUpValidationEvent_ = (event) => {
   };
 
 };
-/* END - Event Validation */
+// END - Event Validation
 
-const createEventsListeners = () => {
+const createFormEventsListeners = () => {
   formListeners('firstName', isValidName);
   formListeners('lastName', isValidSurname);
   formListeners('email', isValidEmail);
@@ -78,4 +56,4 @@ const createEventsListeners = () => {
   elements.signUp.addEventListener("click", signUpValidationEvent_);
 };
 
-createEventsListeners();
+createFormEventsListeners();
--- starterOnly/Js/modal.js
+++ starterOnly/Js/modal.js
@@ -1,3 +1,29 @@
+// BEGIN - DOM ELEMENTS
+const elements = {
+  modalForm : document.querySelector(".modal-body form"),
+  modalbg: document.querySelector(".bground"),
+  modalBtn: document.querySelectorAll(".modal-btn"),
+  modalClosebtn: document.querySelector(".btn-close"),
+  modalCross: document.querySelector(".close"),
+  formData: document.querySelectorAll(".formData"),
+  firstName : document.getElementById("first"),
+  lastName : document.getElementById("last"),
+  email : document.getElementById("email"),
+  birthDate : document.getElementById("birthdate"),
+  quantityTournament : document.getElementById("quantity"),
+  checkCity : document.querySelectorAll(".city-location"),
+  signUp : document.querySelector(".btn-submit"),
+  error : {
+    firstName : document.querySelector("p.error-fName"),
+    lastName : document.querySelector("p.error-lName"),
+    email : document.querySelector("p.error-eMail"),
+    birthDate : document.querySelector("p.error-bDate"),
+    city : document.querySelector("p.error-city"),
+    quantityTournament : document.querySelector("p.error-qValue")
+  }
+};
+// END - DOM ELEMENTS
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,24 +33,22 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const modalCross = document.querySelector(".close");
-const formData = document.querySelectorAll(".formData");
+// BEGIN - launch modal event
+const launchModalEvent_ = (btn) => btn.addEventListener("click", launchModal = () => {
+  elements.modalbg.style.display = "block";
+});
+// END - launch modal event
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// BEGIN - close modal form
+const modalClose_ = () => {
+  elements.modalbg.style.display = "none";
+}
+// END - close modal form
 
-// close modal form
-const modalClosen = () => {
-  modalbg.style.display = "none";
+const createModalEventsListeners = () => {
+  elements.modalBtn.forEach(launchModalEvent_);
+  elements.modalCross.addEventListener("click", modalClose_);
+  elements.modalClosebtn.addEventListener("click",modalClose_);
 }
 
-// close modal event
-modalCross.addEventListener("click", modalClosen);
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+createModalEventsListeners();
