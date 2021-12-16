b3fcbfc197f51f10a564817d9144d5b0062a3b9c
Alex Mant <al.mantione@gmail.com>
2021-12-16 18:17:25 +0100

    maj regexp birthdate bride des dates

--- js/regexp-handler.js
+++ js/regexp-handler.js
@@ -2,4 +2,4 @@ const regExpName = /^[a-z\-\u00C0-\u024F]{2,}$/i;
 const regExpLastName = /^[a-z\-\u00C0-\u024F]{2,}$/i;
 const regExpNumber = /^\d{1,}$/;
 const regExpEmail = /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
-const regExpBirthdate = /^(\d{4})\-(0[1-9]|1[0-2])\-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
+const regExpBirthdate = /^19[00-99]{2}|200[0-7]{1}\-(0[1-9]|1[0-2])\-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
